0b0107f93f288fd07cd4884eda351afcdc365d6b
StudioCrew-design <dev.studiocrew@gmail.com>
2023-06-27 02:27:19 +0530

    Added api method

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 from flask import Flask, render_template
 
-app = Flask("Website")
+app = Flask(__name__)
+
 
 @app.route("/home")
 def home():
-    return render_template("tutorial.html")
-
-@app.route("/about/")
-def about():
-    return render_template("about.html")
+    return render_template("home.html")
 
 
-app.run(debug=True)
+@app.route("/api/v1/<station>/<date>")
+def get_temperature(station, date):
+    temperature = 20
+    return {"station": station,
+            "date": date,
+            "temperature": temperature
+            }
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
